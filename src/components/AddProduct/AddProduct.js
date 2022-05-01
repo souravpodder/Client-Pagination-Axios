d5f021a14cae3ebdb9b28b4e0db78d39a7c740c8
spodder <spodder37@gmail.com>
2022-05-01 19:12:30 +0600

    Pagination added with axios data fetch

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { toast } from "react-toastify";
 const AddProduct = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -5,7 +7,22 @@ const AddProduct = () => {
       name: e.target.name.value,
       price: e.target.price.value,
       image: e.target.image.value,
+      index: e.target.index.value,
+
     };
+
+    try {
+      const { data } = await axios.post('http://localhost:5000/product', product);
+
+      if (!data.success) {
+        return toast.error(data.error);
+      }
+
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error.message)
+    }
+
   };
   return (
     <div className="py-32 px-10 min-h-screen w-full">
@@ -49,6 +66,22 @@ const AddProduct = () => {
             <input
               type="text"
               name="image"
+              placeholder="url"
+              className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 
+                      text-gray-600 placeholder-gray-400
+                      outline-none"
+            />
+          </div>
+          <div className="flex items-center mb-10">
+            <label
+              className="inline-block w-40 mr-6 text-right
+                                    font-bold text-gray-600"
+            >
+              Index
+            </label>
+            <input
+              type="text"
+              name="index"
               placeholder="url"
               className="flex-1 py-2 border-b-2 border-gray-400 focus:border-green-400 
                       text-gray-600 placeholder-gray-400
--- src/components/AllProducts/AllProducts.js
+++ src/components/AllProducts/AllProducts.js
@@ -1,9 +1,93 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const AllProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [totalPage, setTotalPage] = useState(0);
+  // console.log(totalPage);
+  const [limit, setLimit] = useState(5);
+  const [pageNumber, setPageNumber] = useState(0);
+
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(`http://localhost:5000/products?limit=${limit}&pageNumber=${pageNumber}`);
+      console.log(data);
+
+      if (!data.success) {
+        setProducts([]);
+        return toast.error(data.error);
+      }
+
+      setProducts(data.data);
+      setTotalPage(Math.ceil(data.count / limit));
+
+    })();
+  }, [limit, pageNumber])
   return (
-    <div>
-      Manage all products here
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-screen mx-1">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Serial Number
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Product name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Product image
+            </th>
+
+            <th scope="col" className="px-6 py-3">
+              <span className="sr-only">Edit</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            products.map(product => {
+              return <tr key={product._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                  {product.index}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                  {product.name}
+                </th>
+                <td className="px-6 py-4">
+                  {product.price}
+                </td>
+                <td className="px-6 py-4">
+                  <img className='w-20 rounded-lg' src={product.image} alt="" />
+                </td>
+
+                <td className="px-6 py-4 text-right">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+            })
+
+          }
+        </tbody>
+      </table>
+
+      <div className='flex my-3 justify-end'>
+        {
+          [...Array(totalPage).keys()].map(number => <div className={`border-2 cursor-pointer border-black mx-3 px-3 py-2 ${number === pageNumber ? 'bg-black text-white' : ''}`}
+            onClick={() => setPageNumber(number)}
+          >{number + 1}</div>)
+        }
+        <select defaultValue={limit} onChange={(e) => setLimit(e.target.value)}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+      </div>
     </div>
   );
 };
